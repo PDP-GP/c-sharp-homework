ffb25a9021ae6e231ff809d01c1378602345bbcb
Olesya Lenartovich <olesyalenartovich@gmail.com>
2024-10-08 23:19:41 +0300

    Add project files.

---
+++ ConsoleApp1/Task_1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace project
+{
+    class Task_1 //№1.1     простые числа до заданного числа включительно
+    {
+        static void Main()
+        {
+            Console.Write("type a number ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            for (int number = 2; number<=n; number++)
+            {
+                bool ok = true;
+                for (int divider = 2; divider <= Math.Sqrt(number); divider++)
+                {
+                    if (number % divider == 0)
+                    {
+                        ok = false;
+                        break;
+                    }
+
+                    
+                }
+
+                if (ok == true)
+                {
+                    Console.WriteLine(number);
+                }
+
+               
+            }
+            
+            
+        }
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace project
+{
+    class Task_1 //  №1.2  наибольшее и след по величине число из 10
+    {
+        static void Main()
+        {
+            Console.WriteLine("type 10 numbers ");
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("number " + (i+1) + ": ");
+                int chislo = Convert.ToInt32(Console.ReadLine());
+                array[i] = chislo;
+            }
+
+            Array.Sort(array);
+            Console.WriteLine(array[array.Length-1]);
+            Console.WriteLine(array[array.Length - 2]);
+        }
+    }
+}
