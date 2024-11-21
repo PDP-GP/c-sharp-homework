0150ef11eae12b638550ac3f9311853daff9b83d
Olesya Lenartovich <olesyalenartovich@gmail.com>
2024-11-22 01:44:23 +0300

    task 10 added

---
+++ Task_10/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> lines = ["x\tsin(x)"];
+        for (double i = 0; i < 1; i+=0.1)
+        {
+            lines.Add($"{i:F1}\t{Math.Sin(i):F10}");
+        }
+
+        // Set a variable to the Documents path.
+        string docPath =
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        // Write the string array to a new file named "WriteLines.txt".
+        using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt")))
+        {
+            foreach (string line in lines)
+                outputFile.WriteLine(line);
+        }
+    }
+}
